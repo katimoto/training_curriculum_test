d2201c442a6f336f515c7decae80427168c87d07
katimoto <kati.miyamoto@gmail.com>
2020-09-01 15:29:26 +0900

    Issue4の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -37,9 +37,5 @@ class CalendarsController < ApplicationController
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       @week_days.push(days)
     end
-
-    def plan_params
-      params.require(:date)
-    end
   end
 end
